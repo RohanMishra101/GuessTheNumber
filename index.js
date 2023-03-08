6ae795dd72cd2cff2c83e74b45e308f70c480a0d
Rohan Mishra <mishrarohan090@gmail.com>
2023-03-08 20:11:44 +0545

    Home page done

--- index.js
+++ index.js
@@ -1,59 +1,9 @@
 // alert(); function doesnot return any value
 // confirm(); function return a certain value
-let palyGame = confirm("do you want to play a Game!!");
 
-if(palyGame){
-  console.log(window.location.href);
-  // Navigate to a different HTML page
-  window.location.href = "GuessTheNumber.html";
+function guessTheNumPage(){
+    window.location.href = "GuessTheNumber.html";
 }
-
-
-
-
-// function myFunction() {
-//     var x = 5;
-//     if (true) {
-//       var x = 10;
-//       console.log(x); // 10
-//     }
-//     console.log(x); // 10
-// }
-  
-// myFunction();
-
-
-// function myFunction() {
-//     let x = 5;
-//     if (true) {
-//         let x = 10;
-//         console.log(x); // 10
-//     }
-//     console.log(x); // 5
-//   }
-  
-// myFunction();
-
-// function myFunction() {
-//     const x = 5;
-//     if (true) {
-//         const x = 10;
-//         console.log(x); // 10
-//     }
-//     console.log(x); // 5
-//   }
-// myFunction();
-
-// let person = {
-//   name: "John",
-//   age: 27,
-//   isStudent: true
-// };
-
-
-// let age = 27;
-// age = "twenty-seven";
-
 
 
 
