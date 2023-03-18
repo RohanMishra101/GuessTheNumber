3fb4fd4aa4326a67a80afb65bfc76347aad226ca
RohanMishra <76943174+RohanMishra101@users.noreply.github.com>
2023-03-18 12:42:05 +0545

    few changes

--- hangman.js
+++ hangman.js
@@ -13,6 +13,8 @@ function myClick(){
 }
 
 // Gameplay Code
+
+
 // Movies array
 const movies = [
     "The Godfather",
@@ -108,9 +110,27 @@ const animals = [
     "Puma",
     "Jaguar"
 ]; 
+// Mixed array
+
+movies.sort(() => Math.random() - 0.5);
+countriesCities.sort(() => Math.random() - 0.5);
+sports.sort(() => Math.random() - 0.5);
+animals.sort(() => Math.random() - 0.5);
+
+
+//Gusses limit
+var guesses = 0;
+var maxGuesses = 6;
+var guessedLetters = [];
+var _givenWord
+// Concatenate the arrays together
+const mixed = movies.concat(countriesCities, sports, animals);
+
 
 var _getWord = document.querySelector('.givenWord');
 var _getKeyWord = document.querySelector('.button');
+var textbox = document.getElementById("guess_word");
+var _wordCount = document.querySelector('#WordCount');
 
 function getRandNum(){
     var randomNumber = Math.floor(Math.random() * 20);
@@ -121,10 +141,31 @@ function countryCity(){
     themeScreen.style.display = 'none';
     gameMain.style.display = 'block';
     var _randNum = getRandNum();
-    var _givenWord = countriesCities[_randNum];
+    _givenWord = countriesCities[_randNum].toUpperCase();
     console.log(_givenWord.length);
     // var underScores = "_".repeat(_givenWord.length);
-    var underScores = _givenWord.replace(/[a-z]/gi, '_ ');
+    // var underScores = _givenWord.replace(/[a-z]/gi, '_ ');
+    var underScores = _givenWord.replace(/[a-z]/gi, '_').replace(/\s/g, '  ');
+    _wordCount.textContent = "Word Count : " + _givenWord.length;
     _getWord.textContent = underScores;
     console.log(_givenWord);
 }
+
+
+
+// Function to check if the guessed letter is correct
+function checkGuess(){
+    var textboxValue = textbox.value.toUpperCase();
+    console.log(textboxValue);
+    
+    for(var i = 0; i < _givenWord.length; i++){
+        if(textboxValue == _givenWord[i]){
+            
+        }
+    }
+
+    // if(textboxValue == _givenWord){
+    //     alert("You Won!! The wor was" + _givenWord);
+    // }
+}
+
