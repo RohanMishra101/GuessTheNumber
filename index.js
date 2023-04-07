c63fab13b36014d456c8cb29ed1af730dbd69fe5
Rohan Mishra <mishrarohan090@gmail.com>
2023-04-08 00:27:42 +0545

    Home page redesigned completed

--- index.js
+++ index.js
@@ -8,7 +8,54 @@ function guessTheNumPage(){
 function hangMan(){
     window.location.href = "hangman.html";
 }
+function todo(){
+    window.location.href = "todo.html";
+}
 
 
+
+// This is a Single-line comment
+
+/* 
+    This is a multi-line comment
+    which comments multiple lines at once
+*/
+
+
+
+
+// let x = 10;
+if (x > 5) {
+  console.log("x is greater than 5");
+} else {
+  console.log("x is less than or equal to 5");
+}
+
+
+
+
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+  
+let result = addNumbers(5, 7);
+console.log(result); // Output: 12
+
+
+
+
+let x = 5;
+let y = 10;
+if (x > 0 && y > 0) { //Logical operator "&&"
+  console.log("Both x and y are positive");
+}
+
+  
+// While Statement 
+let i = 0;
+while (i < 5) {
+  console.log(i);
+  i++;
+}
 
 
